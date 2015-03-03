fb19fdbed185b3001ab143d6c74b74ca38ce165b
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-03-03 17:58:23 +0800

    Enable commenting on posts and upvote on comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ var angular = require('angular');
 
 var app = angular.module('happyPosts', ['ui.router','ui.bootstrap']);
 
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider){
 
     $stateProvider.state('home', {
       url: '/home',
@@ -11,16 +11,22 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       controller: 'MainCtrl'
     });
 
+    $stateProvider.state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
     $urlRouterProvider.otherwise('home');
-}]);
+});
 
 app.factory('posts', function(){
   var p = {posts: [
-    {title: 'Post 1', upvotes: 5},
-    {title: 'Post 2', upvotes: 2},
-    {title: 'Post 3', upvotes: 15},
-    {title: 'Post 4', upvotes: 9},
-    {title: 'Post 5', upvotes: 4}
+    {title: 'Post 1', upvotes: 5, comments:[]},
+    {title: 'Post 2', upvotes: 2, comments:[]},
+    {title: 'Post 3', upvotes: 15, comments:[]},
+    {title: 'Post 4', upvotes: 9, comments:[]},
+    {title: 'Post 5', upvotes: 4, comments:[]}
     ]};
   return p;
 });
@@ -37,9 +43,35 @@ app.controller('MainCtrl', function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0});
+      upvotes: 0,
+      comments: [
+        // {author: 'test01', body: 'this is a fake comment 01', upvotes: 0},
+        // {author: 'test02', body: 'this is a fake comment 02', upvotes: 0}
+      ]
+    });
     $scope.title='';
     $scope.link='';
+  };
+
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  };
+
+});
+
+app.controller('PostsCtrl', function($scope,$stateParams,posts){
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === ''){
+      return;
+    }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
   };
 
   $scope.incrementUpvotes = function(post){
